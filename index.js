63795461385c52f17e6547a2b131c7cb5e54601d
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-27 14:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function the BeatlesPlay(arraym,arrayi){
   var array
+  for (let i=0;i<arraym.length;i++){
+    array[i]='${arraym[i]} plays ${arrayi[i]'
+  }
+  return array
 }
