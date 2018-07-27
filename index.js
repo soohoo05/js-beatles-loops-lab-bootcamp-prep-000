4efa7a352aec63081bfe34d3604a76f5969494a1
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-27 14:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(arraym,arrayi){
   var array=[]
   for (let i=0;i<arraym.length-1;i++){
-    array.push(arraym[i]+" plays "+array[i])
+    array.push(arraym[i]+" plays "+arrayi[i])
   }
   return array
 }
