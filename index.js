eab6220a84f859482055764c8d229283354dc7da
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-27 14:28:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(arraym,arrayi){
   var array=[]
-  for (let i=0;i<arraym.length-1;i++){
+  for (let i=0;i<arraym.length;i++){
     array.push(arraym[i]+" plays "+arrayi[i])
   }
   return array
