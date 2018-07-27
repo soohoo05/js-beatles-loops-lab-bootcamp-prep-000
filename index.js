2f95b29a63290bb0ced005ed6c4a25f3072ad5fb
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-27 14:26:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(arraym,arrayi){
   var array
-  for (let i=0;i<arraym.length;i++){
+  for (let i=0;i<arraym.length-1;i++){
     array[i]='${arraym[i]} plays ${arrayi[i]'
   }
   return array
